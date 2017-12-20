a596061cdc6ebf16cade79c83fbc7e8b4afe8f57
Ethan <ethanyt@qq.com>
2017-12-20 15:18:57 +0800

    a

--- server.js
+++ server.js
@@ -17,44 +17,6 @@ var command = [
 ];
 
 
-function ssh(branchName) {
-  var Client = require('ssh2').Client;
-  var conn = new Client();
-  conn.on('ready', function () {
-    console.log('Client :: ready');
-    command[1] += branchName;
-    conn.exec(command.join('&&'), function (err, stream) {
-      if (err) {
-        response.writeHead(500);
-        response.end('Server Internal Error.\r\n' + err.message);
-        console.log('ERROR: ' + err.message)
-        conn.end();
-      }
-      stream.on('close', function (code, signal) {
-        console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
-        if (code == 0) {
-          response.writeHead(200);
-          response.end('Deploy Done.');
-        } else {
-          response.writeHead(500);
-          response.end('Server Internal Error.');
-        }
-        conn.end();
-      }).on('data', function (data) {
-        console.log('STDOUT: ' + data);
-      }).stderr.on('data', function (data) {
-        console.log('STDERR: ' + data)
-      });
-    });
-  }).connect({
-    host: '138.68.43.201',
-    username: 'root',
-    privateKey: require('fs').readFileSync('/webhook-key/key')
-  });
-}
-
-
-
 var deployServer = http.createServer(function (request, response) {
   if (request.url.search(/deploy\/?$/i) > 0) {
 
@@ -78,7 +40,39 @@ var deployServer = http.createServer(function (request, response) {
 
       var reg = /heads\/(.*)/;
       var branchName = reg.exec(json.ref)[1];
-      ssh(branchName);
+      var Client = require('ssh2').Client;
+      var conn = new Client();
+      conn.on('ready', function () {
+        console.log('Client :: ready');
+        command[1] += branchName;
+        conn.exec(command.join('&&'), function (err, stream) {
+          if (err) {
+            response.writeHead(500);
+            response.end('Server Internal Error.\r\n' + err.message);
+            console.log('ERROR: ' + err.message)
+            conn.end();
+          }
+          stream.on('close', function (code, signal) {
+            console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+            if (code == 0) {
+              response.writeHead(200);
+              response.end('Deploy Done.');
+            } else {
+              response.writeHead(500);
+              response.end('Server Internal Error.');
+            }
+            conn.end();
+          }).on('data', function (data) {
+            console.log('STDOUT: ' + data);
+          }).stderr.on('data', function (data) {
+            console.log('STDERR: ' + data)
+          });
+        });
+      }).connect({
+        host: '138.68.43.201',
+        username: 'root',
+        privateKey: require('fs').readFileSync('/webhook-key/key')
+      });
     });
 
   } else {
