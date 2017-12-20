abb0a7c835eeb8f72b7c413f2d77ae021c011fc7
Ethan <ethanyt@qq.com>
2017-12-20 13:46:46 +0800

    a

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 "use strict";
 var http = require('http');
+var querystring = require('querystring');
 
-const PORT = process.env.PORT || 18340
-  , PATH = process.env.DIR || "~/node-test";
+const PORT = process.env.PORT || 18340,
+  PATH = process.env.DIR || "~/node-test";
 
 var command = [
   'cd ' + PATH,
@@ -17,39 +18,60 @@ var command = [
 
 var deployServer = http.createServer(function (request, response) {
   if (request.url.search(/deploy\/?$/i) > 0) {
-    var Client = require('ssh2').Client;
-    var conn = new Client();
-    conn.on('ready', function () {
-      console.log('Client :: ready');
-      conn.exec(command, function (err, stream) {
-        if (err) {
-          response.writeHead(500);
-          response.end('Server Internal Error.\r\n' + err.message);
-          console.log('ERROR: ' + err.message)
-          conn.end();
-        }
-        stream.on('close', function (code, signal) {
-          console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
-          if (code == 0) {
-            response.writeHead(200);
-            response.end('Deploy Done.');
-          }
-          else{
-            response.writeHead(500);
-            response.end('Server Internal Error.');
-          }
-          conn.end();
-        }).on('data', function (data) {
-          console.log('STDOUT: ' + data);
-        }).stderr.on('data', function (data) {
-          console.log('STDERR: ' + data)
-        });
-      });
-    }).connect({
-      host: '138.68.43.201',
-      username: 'root',
-      privateKey: require('fs').readFileSync('/root/.ssh/id_rsa')
+
+    // 定义了一个post变量，用于暂存请求体的信息
+    var post = '';
+
+    // 通过req的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
+    request.on('data', function (chunk) {
+      post += chunk;
     });
+
+
+
+    request.on('end', function () {
+      post = querystring.parse(post);
+
+      var reg = /heads\/(.*)/;
+      
+      console.log(reg.exec(post.ref)[1]);
+      
+
+      var Client = require('ssh2').Client;
+      var conn = new Client();
+      conn.on('ready', function () {
+        console.log('Client :: ready');
+        conn.exec(command, function (err, stream) {
+          if (err) {
+            response.writeHead(500);
+            response.end('Server Internal Error.\r\n' + err.message);
+            console.log('ERROR: ' + err.message)
+            conn.end();
+          }
+          stream.on('close', function (code, signal) {
+            console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+            if (code == 0) {
+              response.writeHead(200);
+              response.end('Deploy Done.');
+            } else {
+              response.writeHead(500);
+              response.end('Server Internal Error.');
+            }
+            conn.end();
+          }).on('data', function (data) {
+            console.log('STDOUT: ' + data);
+          }).stderr.on('data', function (data) {
+            console.log('STDERR: ' + data)
+          });
+        });
+      }).connect({
+        host: '138.68.43.201',
+        username: 'root',
+        privateKey: require('fs').readFileSync('/root/.ssh/id_rsa')
+      });
+
+    });
+
 
 
 
